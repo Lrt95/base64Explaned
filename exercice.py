ea88f35098db92a35804a22084207a9314bf5552
ilias <iliasguechi@gmail.com>
2021-03-29 14:26:11 +0200

    exercice wip test

--- exercice.py
+++ exercice.py
@@ -3,12 +3,16 @@
 Created by Antony Correia
 Python Docstring
 """
-
+def listToString(list):
+    result = "".join(list)
+    print(result)
 
 def main():
     """ Function Main"""
-    print("hello")
+    myList = ['B', "ib", 'a', "bou"]
+    listToString(myList)
 
 
 if __name__ == '__main__':
     main()
+

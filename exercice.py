e48e9248bbab82bf664fa8083a5e61433e4dadc0
CHALET <emerick.chalet@edu.itescia.fr>
2021-03-29 14:40:55 +0200

    Update exercice.py
    exercices 1.2 et 3

--- exercice.py
+++ exercice.py
@@ -4,11 +4,27 @@ Created by Antony Correia
 Python Docstring
 """
 
+def string_to_list(text):
+    """ Function string_to_list
+    transform string to list of characters"""
+    my_list = []
+    for character in text:
+        my_list.append(character)
+    return my_list
 
-def main():
-    """ Function Main"""
-    print("hello")
+def character_to_ASCII(character_list):
+    """Function character_to_ASCII
+    transform characters to ASCII"""
+    for i in range(len(character_list)):
+        character_list[i] = ord(character_list[i])
+    return character_list
 
+def ASCII_to_binary(ASCII_list):
+    """ Function ASCII_to_binary
+    transform ASCII to binary"""
+    for i in range(len(ASCII_list)):
+        ASCII_list[i] = bin(ASCII_list[i])[2::1]
+    return ASCII_list
 
 if __name__ == '__main__':
-    main()
+    print(ASCII_to_binary(character_to_ASCII(string_to_list('ABCDE'))))

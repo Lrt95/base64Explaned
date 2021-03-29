445d3024817824b0e62a007a7e66bfdffaac68aa
ilias <iliasguechi@gmail.com>
2021-03-29 14:35:41 +0200

    exercice 10 et 11

--- exercice.py
+++ exercice.py
@@ -5,13 +5,20 @@ Python Docstring
 """
 def listToString(list):
     result = "".join(list)
-    print(result)
+    return(result)
+
+def setMultipleOfString(str, mult):
+    while len(str) % mult != 0:
+        str = str + '='
+    return(str)
 
 def main():
     """ Function Main"""
-    myList = ['B', "ib", 'a', "bou"]
-    listToString(myList)
-
+    myList = ['B', "ib", 'a', "bo"]
+    myString = listToString(myList)
+    myTabString = setMultipleOfString(myString, 4)
+    print("Exercice 10: " + myString)
+    print("Exercice 11: " + myTabString)
 
 if __name__ == '__main__':
     main()

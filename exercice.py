a0603ddae534bee76da6b6eef821bc50dae9d706
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-03-29 15:05:09 +0200

    fonction 5
    fonction 5

--- exercice.py
+++ exercice.py
@@ -4,6 +4,7 @@ Created by Antony Correia
 Python Docstring
 """
 
+
 def string_to_list(text):
     """ Function string_to_list
     transform string to list of characters"""
@@ -12,12 +13,14 @@ def string_to_list(text):
         my_list.append(character)
     return my_list
 
+
 def character_to_ASCII(character_list):
     """Function character_to_ASCII
     transform characters to ASCII"""
     for i in range(len(character_list)):
         character_list[i] = ord(character_list[i])
     return character_list
+
 
 def ASCII_to_binary(ASCII_list):
     """ Function ASCII_to_binary
@@ -26,5 +29,20 @@ def ASCII_to_binary(ASCII_list):
         ASCII_list[i] = bin(ASCII_list[i])[2::1]
     return ASCII_list
 
+
+def add_zero(binary_list):
+    """add_zero
+    add zero in first position"""
+    for i in range(len(binary_list)):
+        binary_list[i] = "0" + binary_list[i]
+    return binary_list
+
+def concat_list_string(list):
+    list_res = ""
+    for i in range(len(list)):
+        list_res += list[i]
+    return list_res
+
+
 if __name__ == '__main__':
-    print(ASCII_to_binary(character_to_ASCII(string_to_list('ABCDE'))))
+    print(concat_list_string(add_zero(ASCII_to_binary(character_to_ASCII(string_to_list('ABCDE'))))))
